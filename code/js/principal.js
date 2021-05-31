08a1c91db70c28e6c271c706625ebda19b11130f
KauanValle <kauanvalle3@gmail.com>
2021-05-31 01:44:01 -0300

    Site completo

---
+++ code/js/principal.js
@@ -0,0 +1,57 @@
+function cronometro(){//Função em que calcula a hora atual e dispara ela no display do site.
+    var result = document.getElementById('horas')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    var segundos = data.getSeconds()
+
+    if(hora < 10){hora = '0'+hora}
+    if(minuto < 10){minuto = '0'+minuto}
+    if(segundos < 10){segundos = '0'+segundos}
+
+    var time = hora+":"+minuto+":"+segundos
+    result.innerHTML = time
+}
+function time(){ //Intervalo de tempo em que a hora é atualizada
+    setInterval(cronometro, 500)
+}
+
+//-------------------------VARIAVEIS---------------------------------------
+
+    var data = new Date()
+    var horas = data.getHours()
+    var title = document.getElementById('title')
+    var fundo = document.getElementById('body')
+    var imgs = document.getElementById('img')
+    var creditos = document.getElementById('creditos')
+
+    //---------------------------DIA-----------------------------------
+
+    if(horas <= 12 && horas > 5 ){
+        fundo.style = 'background-color: #88ffcd;'
+        title.style = 'color: black;'
+        creditos.style = 'color: black;'
+        imgs.src = 'images/manhã.jpg'
+
+            //---------------------------TARDE-----------------------------------
+
+            }else if(horas >= 13 && horas <= 18){
+                fundo.style = 'background-color: #ffac2fb4;'
+                title.style = 'color: black;'
+                creditos.style = 'color: black;'
+                imgs.src = 'images/tarde.jpg'
+
+                //---------------------------NOITE-----------------------------------
+
+                }else if(horas <= 23 && horas > 5){
+                        fundo.style = 'background-color: #0c0b27;'
+                        title.style = 'color: white;'
+                        imgs.src = 'images/noite.jpg'
+
+                //---------------------------MADRUGADA-----------------------------------
+
+                        }else{
+                            fundo.style = 'background-color: #1f1f1f;'
+                            title.style = 'color: white;'
+                            imgs.src = 'images/madrugada.jpg'
+                        }
